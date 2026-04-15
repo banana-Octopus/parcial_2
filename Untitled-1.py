7b3df1f180a26f840d2e9ddec12f34079060c32a
banana-Octopus <sar.quitral@duocuc.cl>
2026-04-15 09:23:23 -0400

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,2 @@
+Edad = int(input("dime tu edad, por favor"))
+print ("tu edad es: ", Edad)
---
+++ variable_enteras 42 #entero (int).py
@@ -0,0 +1,7 @@
+variable_enteras = 42 #entero (int)
+variable_decimales = 3.14 #decimal (decimal)
+variable_texto = 'jand'
+variable_texto = 'maria'
+variable_logica = True
+
+print ("hola, ", variable_texto)
