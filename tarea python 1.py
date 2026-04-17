418bd78b434f79fcd54dc8071abdf182d74f08df
banana-Octopus <sar.quitral@duocuc.cl>
2026-04-17 01:08:39 -0400

    Add files via upload

---
+++ tarea python 1.py
@@ -0,0 +1,42 @@
+"""
+usuario correcto: Duoc
+
+contraseña:123444pollo
+"""
+correct_password=("123444pollo")
+max_intentos=3
+intento=0
+
+usuario = input("Ingrese el usuario ")
+
+  
+if usuario == ("Duoc"):
+  print("Usuario correcto")
+
+  password = input("ingrese la contraseña: ")
+
+  if password == correct_password:
+        print("Ingreso correctamente, ¡bienvenido!")
+   
+
+  else:
+   while True:
+    print ("Contraseña incorrecta")
+    
+    password = input("Password: ").strip()
+    intento += 1
+
+    if intento >= max_intentos:
+        print ("usuario bloqueado")
+        break
+
+    
+    else:
+      
+     print ( f"intentos restantes: . {max_intentos - intento}. ")
+     if password == correct_password:
+            print("Ingreso correctamente, ¡bienvenido!")
+ 
+
+else:
+    print("Usuario incorrecto, acceso denegado")
