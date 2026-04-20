5dacf4368e0bce53196235aa7a37adcc84e9c82a
banana-Octopus <sar.quitral@duocuc.cl>
2026-04-20 11:12:40 -0400

    Add files via upload

---
+++ clase 20-04.py
@@ -0,0 +1,51 @@
+""""
+numero = [1,2,3,4,5]
+
+print (numero[0:5])
+escribe los numeros del 1 al 5
+"""""
+# otro coso
+
+"""""
+numero.append(50)
+
+print (numero)
+"""""
+
+# tupla
+
+""""
+objetos = ("lentes","1","cereza")
+
+print (objetos[0])
+"""
+# ejemplo 2 tupla
+"""""
+colores = ("amarillo","rojo","azul")
+
+print (colores[1])
+"""""
+#otra actividad
+"""""
+Cosa ={
+    "juan":25,
+    "maria":20,
+    "carlos":19,
+}
+print("carlos tiene una edad de : ", Cosa["carlos"], "hoola, juan tienes una edad de:", Cosa["juan"])
+"""""
+# ultimo apredisaje
+"""""
+def saludo(nombre):
+    return f"¡hola!,{nombre}"
+mensaje = saludo("estudicante: ")
+print(mensaje)
+"""""
+# otro formato owo
+def saludo(nombre):
+    return "¡hola!", nombre + "!"
+mensaje = saludo("bienvenido: ")
+
+nombre = input("dime tu nombre: ")
+
+print(f"{mensaje}, {nombre}")
